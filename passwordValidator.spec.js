8c4d5e8e57db7f246b7b4c4e00edb417243638dd
Mélia Enilorac <melia.enilorac@gmail.com>
2025-12-17 21:28:04 +0100

    ajout de la partie 2 de l'exercice 1

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,11 +1,51 @@
 const validatePassword = require('./passwordValidator');
 
 describe('validatePassword', () => {
-    test('should return true for passwords with at least 8 characters', () => {
-        expect(validatePassword('abcdefgh')).toBe(true);
+    test('should return true for passwords with at least 8 characters, 1 letter and 1 number', () => {
+        expect(validatePassword('abcd1234')).toBe(true);
     });
 
     test('should return false for passwords with less than 8 characters', () => {
-        expect(validatePassword('abc')).toBe(false);
+        expect(validatePassword('abc1')).toBe(false);
+    });
+
+    test('should return false for passwords without letters', () => {
+        expect(validatePassword('12345678')).toBe(false);
+    });
+
+    test('should return false for passwords without numbers', () => {
+        expect(validatePassword('abcdefgh')).toBe(false);
+    });
+
+    // Tests pour la longueur paramétrable
+    test('should allow custom minimum length', () => {
+        expect(validatePassword('abc1', { minLength: 4 })).toBe(true);
+        expect(validatePassword('ab1', { minLength: 4 })).toBe(false);
+    });
+
+    // Tests pour le nombre minimum de lettres
+    test('should allow custom minimum number of letters', () => {
+        expect(validatePassword('aaa12345', { minLetters: 3 })).toBe(true);
+        expect(validatePassword('aa123456', { minLetters: 3 })).toBe(false);
+    });
+
+    // Tests pour le nombre minimum de chiffres
+    test('should allow custom minimum number of digits', () => {
+        expect(validatePassword('abcd1234', { minDigits: 3 })).toBe(true);
+        expect(validatePassword('abcde12', { minDigits: 3 })).toBe(false);
+    });
+
+    // Tests pour les règles personnalisées
+    test('should allow custom validation rules', () => {
+        const hasUpperCase = (password) => /[A-Z]/.test(password);
+        expect(validatePassword('Abcd1234', { customRules: [hasUpperCase] })).toBe(true);
+        expect(validatePassword('abcd1234', { customRules: [hasUpperCase] })).toBe(false);
+    });
+
+    test('should apply multiple custom rules', () => {
+        const hasUpperCase = (password) => /[A-Z]/.test(password);
+        const hasSpecialChar = (password) => /[!@#$%^&*]/.test(password);
+        expect(validatePassword('Abcd123!', { customRules: [hasUpperCase, hasSpecialChar] })).toBe(true);
+        expect(validatePassword('Abcd1234', { customRules: [hasUpperCase, hasSpecialChar] })).toBe(false);
     });
 });
