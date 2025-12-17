8709dec9b6d556eee363e590fa10800aba0be425
Mélia Enilorac <melia.enilorac@gmail.com>
2025-12-17 21:00:02 +0100

    ajout de la vérification de la présence d'au moins un chiffre dans le mot de passe ainsi que son test

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,5 +1,7 @@
 function validatePassword(password) {
-    return password.length >= 8;
+    const hasMinLength = password.length >= 8;
+    const hasNumber = /[0-9]/.test(password);
+    return hasMinLength && hasNumber;
 }
 
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,11 +1,15 @@
 const validatePassword = require('./passwordValidator');
 
 describe('validatePassword', () => {
-    test('should return true for passwords with at least 8 characters', () => {
-        expect(validatePassword('abcdefgh')).toBe(true);
+    test('should return true for passwords with at least 8 characters and 1 number', () => {
+        expect(validatePassword('abcd1234')).toBe(true);
+    });
+
+    test('should return false for passwords with at least 8 characters and no number', () => {
+        expect(validatePassword('abcdefgh')).toBe(false);
     });
 
     test('should return false for passwords with less than 8 characters', () => {
-        expect(validatePassword('abc')).toBe(false);
+        expect(validatePassword('abc1')).toBe(false);
     });
 });
