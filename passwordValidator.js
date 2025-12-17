93bdb0d682997ce12a8c23ac78d35a54b697c1b3
Mélia Enilorac <melia.enilorac@gmail.com>
2025-12-17 21:02:51 +0100

    ajout de la vérification de la présence d'au moins une lettre dans le mot de passe ainsi que son test

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,7 +1,8 @@
 function validatePassword(password) {
     const hasMinLength = password.length >= 8;
+    const hasLetter = /[a-zA-Z]/.test(password);
     const hasNumber = /[0-9]/.test(password);
-    return hasMinLength && hasNumber;
+    return hasMinLength && hasLetter && hasNumber;
 }
 
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,15 +1,19 @@
 const validatePassword = require('./passwordValidator');
 
 describe('validatePassword', () => {
-    test('should return true for passwords with at least 8 characters and 1 number', () => {
+    test('should return true for passwords with at least 8 characters, 1 letter, and 1 number', () => {
         expect(validatePassword('abcd1234')).toBe(true);
+    });
+    
+    test('should return false for passwords with less than 8 characters', () => {
+        expect(validatePassword('abc1')).toBe(false);
     });
 
     test('should return false for passwords with at least 8 characters and no number', () => {
         expect(validatePassword('abcdefgh')).toBe(false);
     });
 
-    test('should return false for passwords with less than 8 characters', () => {
-        expect(validatePassword('abc1')).toBe(false);
+    test('should return false for passwords with at least 8 characters and no letter', () => {
+        expect(validatePassword('12345678')).toBe(false);
     });
 });
