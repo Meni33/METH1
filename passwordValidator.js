93e13a297787e1c8dc2d85053afe3ae3b70aec8f
Mélia Enilorac <melia.enilorac@gmail.com>
2025-12-19 11:13:35 +0100

    Ajout règle validation 8 caractères minimum

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,27 +1,8 @@
-function validatePassword(password, options = {}) {
-    // Options par défaut
-    const {
-        minLength = 8,
-        minLetters = 1,
-        minDigits = 1,
-        customRules = []
-    } = options;
-
-    // Vérification de la longueur
-    const hasLength = password.length >= minLength;
-
-    // Vérification du nombre de lettres
-    const letterMatches = password.match(/[a-zA-Z]/g);
-    const hasEnoughLetters = letterMatches ? letterMatches.length >= minLetters : false;
-
-    // Vérification du nombre de chiffres
-    const digitMatches = password.match(/[0-9]/g);
-    const hasEnoughDigits = digitMatches ? digitMatches.length >= minDigits : false;
-
-    // Vérification des règles personnalisées
-    const passesCustomRules = customRules.every(rule => rule(password));
-
-    return hasLength && hasEnoughLetters && hasEnoughDigits && passesCustomRules;
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    return true;
 }
 
 module.exports = validatePassword;
