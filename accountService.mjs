c4762b45cbdfd17e70b582e9a4e893a093bd5778
Mélia Enilorac <melia.enilorac@gmail.com>
2026-01-05 22:00:23 +0100

    Initialisation du dossier CRUD
    - Création du dossier crud
    - Ajout des fichiers de base du CRUD (Account, DAO, service, client, database)

---
+++ accountService.mjs
@@ -0,0 +1,12 @@
+import { Account } from "./account.mjs";
+import { accountDAO } from "./accountDAO.mjs";
+
+export const accountService = {
+  addAccount(lastName, firstName) {
+    const account = new Account(undefined, lastName, firstName);
+    accountDAO.insertAccount(account);
+  },
+  getAccountList() {},
+  saveAccount(id, lastName, firstName) {},
+  getAccount(id) {},
+};
